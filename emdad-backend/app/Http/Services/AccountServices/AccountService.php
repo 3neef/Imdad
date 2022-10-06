dad3f0381bc0a4a23eb9bf76b3a8be41104d09aa
mohamed.osman <mohamed.osman@nctr.sd>
2022-10-06 08:32:14 +0200

    migration and company controller

---
+++ emdad-backend/app/Http/Services/AccountServices/AccountService.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Services\AccountServices;
+
+class AccountService{
+
+    public function createCompany($request)
+    {
+        # code...
+
+        // after save make an observer to create user for the company
+    }
+
+
+}
