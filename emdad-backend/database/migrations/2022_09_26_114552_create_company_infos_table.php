3729c4c40b2988c19f5be9f37cd9a39d903bdf88
mohamed.osman <mohamed.osman@nctr.sd>
2022-10-06 08:33:23 +0200

    company info table rename

--- emdad-backend/database/migrations/2022_09_26_114552_create_company_infos_table.php
+++ emdad-backend/database/migrations/2022_09_26_114552_create_company_infos_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('company_infos', function (Blueprint $table) {
+        Schema::create('company_info', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string("name",100)->unique();
