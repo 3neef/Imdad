f080fa7d3df212a65019773588e57d41da643523
mohamed.osman <mohamed.osman@nctr.sd>
2022-10-06 08:32:55 +0200

    company migrations and  controllers

--- emdad-backend/database/migrations/2022_09_06_062310_create_cities_table.php
+++ emdad-backend/database/migrations/2022_09_06_062310_create_cities_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('company_locations', function (Blueprint $table) {
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->foreignId("company_id")->constrained();
-            $table->foreignId("city_id")->constrained();
-            $table->string("address_name",255);
-            $table->string("address_details",255);
-            $table->string("address_contact_phone",15);
+            $table->string("name_ar");
+            $table->string("name_en");
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('company_locations');
+        Schema::dropIfExists('cities');
     }
 };
--- emdad-backend/database/migrations/2022_09_26_114552_create_company_infos_table.php
+++ emdad-backend/database/migrations/2022_09_26_114552_create_company_infos_table.php
@@ -16,6 +16,13 @@ return new class extends Migration
         Schema::create('company_infos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("name",100)->unique();
+            $table->string("company_id",25)->unique();
+            $table->tinyInteger("company_type")->default(0)->comment("0=emdad,1=buyer,2=supplier");
+            $table->string("company_vat_id",25)->unique();
+            $table->string("contact_name",100)->nullable(false);
+            $table->string("contact_phone",15)->nullable(false);
+            $table->string("contact_email",100)->nullable(false);
         });
     }
 
