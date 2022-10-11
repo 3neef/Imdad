a6c179ad83372b1caa7da7f0bf73b1d0786627ca
mohamed osman <mohamed.osman@nctr.sd>
2022-10-11 14:18:05 +0200

    company migration

--- emdad-backend/database/migrations/2022_09_26_114552_create_company_infos_table.php
+++ emdad-backend/database/migrations/2022_09_26_114552_create_company_infos_table.php
@@ -17,12 +17,18 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string("name",100)->unique();
+            $table->string("logo_path",50)->unique()->nullable(true);
+            $table->string("cr_path",50)->unique()->nullable(true);
+            $table->string("vat_path",50)->unique()->nullable(true);
             $table->string("company_id",25)->unique();
             $table->tinyInteger("company_type")->default(0)->comment("0=emdad,1=buyer,2=supplier");
             $table->string("company_vat_id",25)->unique();
+            $table->string("company_cr_id",25)->unique();
             $table->string("contact_name",100)->nullable(false);
             $table->string("contact_phone",15)->nullable(false);
+            $table->boolean("is_validated")->default(false);
             $table->string("contact_email",100)->nullable(false);
+            $table->foreignId('created_by')->references("id")->on("users")->restrictOnDelete()->restrictOnUpdate();
             $table->softDeletes();
         });
     }
