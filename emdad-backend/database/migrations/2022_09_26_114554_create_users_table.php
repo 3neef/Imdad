a52f8d33a108dfc9bf614c2f0e657fcca244129c
omer.hussien <omer.hussien@nctr.sd>
2022-10-17 13:05:34 +0200

    last edit in migration

--- emdad-backend/database/migrations/2022_09_26_114554_create_users_table.php
+++ emdad-backend/database/migrations/2022_09_26_114554_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->unsignedBigInteger('company_id')->nullable();
             $table->unsignedBigInteger('role_id')->nullable();
             $table->unsignedBigInteger('company_id')->nullable();
             $table->rememberToken();
