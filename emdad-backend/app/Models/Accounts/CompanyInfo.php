7c02d3d8f40038c3444e57dc0ea6fd23108fee25
Marwan <marwan.salah@nctr.sd>
2022-10-18 12:01:56 +0200

    adding subscription packages

--- emdad-backend/app/Models/Accounts/CompanyInfo.php
+++ emdad-backend/app/Models/Accounts/CompanyInfo.php
@@ -11,6 +11,8 @@ class CompanyInfo extends Model
     use HasFactory,SoftDeletes;
     protected $table = 'company_info';
     protected $fillable = [
-        'name', 'company_id', 'company_type', 'company_vat_id', 'contact_name', 'contact_phone', 'contact_email','subscription_details','cr_expire_data'
+        'name', 'company_id', 'company_type', 'company_vat_id', 
+        'contact_name', 'contact_phone', 'contact_email',
+        'subscription_details','cr_expire_data', 'subs_id', 'subscription_details'
     ];
 }
