069c973cc3b392a4dfffae04f1120c674078d2bc
sidahmed.faisal <sidahmed.faisal@nctr.sd>
2022-11-03 13:38:29 +0200

    pushing tests 3 nov

--- emdad-backend/tests/Feature/Auth/AuthControllerTest.php
+++ emdad-backend/tests/Feature/Auth/AuthControllerTest.php
@@ -17,50 +17,103 @@ class AuthControllerTest extends TestCase
         $response = $this->call('post','api/v1_0/users/register',[
             "name"=>'ahmed',
             "password"=>"ahmedpass",
-            "email"=>"osamaali.osali@gmail.com",
-            "mobile"=>"012909892783",
-            "roleId"=> "anything,1",
-            "companyId"=> "anything,1"
+            "email"=>"sidahmedfaisal.sf@gmail.com",
+            "mobile"=>"0916619166",
+            "defaultCompany"=> "1"
         ]);
         $response->assertStatus(200);
     }
 
-    // public function testcreateUserToCompany(){
+    public function testcreateUserToCompany(){
+        $response = $this->call('post','api/v1_0/users/createUser',[
+            "name"=>'ahmed',
+            "password"=>"ahmedpass",
+            "email"=>"sidahmedfaisal.sf@gmail.com",
+            "mobile"=>"0916619166",
+            "roleId"=> "1",
+            "companyId"=>"1"
+        ]);
+        $response->assertStatus(200);
+    }
 
-    // }
-    // public function testloginUser(){
-
-    // }
-    // public function testactivateUser(){
-
-    // }
-    // public function testlogoutUser(){
-
-    // }
-    // public function testupdateUser(){
-
-    // }
-    // public function testdeleteUser(){
-
-    // }
-    // public function testrestoreUser(){
-    // }
-    // public function testforgotPassword(){
-    //     //
-    // }
-    // public function testresetPassword(){
-    //     //
-    // }
-    // public function testassignRole(){
-    //     //
-    // }
-    // public function testunAssignRole(){
-    //     //
-    // }
-    // public function testrestoreOldRole(){
-    //     //
-    // }
-    // public function testsetDefaultCompany(){
-    //     //
-    // }
+    public function testloginUser(){
+        $response = $this->call('post','api/v1_0/users/login',[
+            "email"=>"sidahmedfaisal.sf@gmail.com",
+            "password"=>"ahmedpass",
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testactivateUser(){
+        $response = $this->call('post','api/v1_0/users/activate',[
+            "id"=>"1",
+            "otp"=>"asasjlk",
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testlogoutUser(){
+        $response = $this->call('post','api/v1_0/users/logout');
+        $response->assertStatus(200);
+    }
+    public function testupdateUser(){
+        $response = $this->call('post','api/v1_0/users/update',[
+            "name"=>'ahmed',
+            "password"=>"ahmedpass",
+            "email"=>"sidahmedfaisal.sf@gmail.com",
+            "mobile"=>"0916619166",
+            "roleId"=> "1",
+            "companyId"=>"1"
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testdeleteUser(){
+        $response = $this->call('delete','api/v1_0/users/delete/1');
+        $response->assertStatus(301);
+    }
+    public function testrestoreUser(){
+        $response = $this->call('put','api/v1_0/users/restore/1');
+        $response->assertStatus(200);
+    }
+    public function testforgotPassword(){
+        $response = $this->call('put','api/v1_0/users/forgot-password/1',[
+        "email" =>"ahmed@gmail.com"
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testresetPassword(){
+        $response = $this->call('put','api/v1_0/users/reset-password',[
+            'email' => 'sidahmed@gmail.com',
+            'oldPassword' => 'dsklfjlsjkla',
+            'newPassword' => 'sdlfkajkljsl'
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testassignRole(){
+        $response = $this->call('post','api/v1_0/users/assginRole',[
+            'userId' => '1',
+            'role' => 'anything',
+            'companyId'=> '1'
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testunAssignRole(){
+        $response = $this->call('post','api/v1_0/users/unAssginRole',[
+            'userId' => '1',
+            'companyId'=> '1'
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testrestoreOldRole(){
+        $response = $this->call('post','api/v1_0/users/oldRole',[
+            'userId' => '1',
+            'companyId'=> '1'
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testsetDefaultCompany(){
+        $response = $this->call('post','api/v1_0/users/setDefaultCompany',[
+            'userId' => '1',
+            'companyId'=> '1'
+        ]);
+        $response->assertStatus(200);
+    }
 }
--- emdad-backend/tests/Feature/Roles/RoleControllerTest.php
+++ emdad-backend/tests/Feature/Roles/RoleControllerTest.php
@@ -19,4 +19,38 @@ class RoleControllerTest extends TestCase
 
         $response->assertStatus(200);
     }
+    public function testsaveRole(){
+        $response = $this->call('post','api/v1_0/permissions/save',[
+            "name"=>"admin",
+            "type"=>"1"
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testgetallRoles(){
+        $response = $this->call('get','api/v1_0/permissions/save',[
+            "name"=>"admin",
+            "type"=>"1"
+        ]);
+        $response->assertStatus(200);
+    }
+    public function testgetByRoleId(){
+        $response = $this->call('get','api/v1_0/permissions/getByRoleId/1');
+        $response->assertStatus(200);
+    }
+    public function testgetByType(){
+        $response = $this->call('get','api/v1_0/permissions/getByType/1');
+        $response->assertStatus(200);
+    }
+    public function updateRole(){
+        $response = $this->call('put','api/v1_0/permissions/update/1');
+        $response->assertStatus(200);
+    }
+    public function deleteRole(){
+        $response = $this->call('delete','api/v1_0/permissions/delete/1');
+        $response->assertStatus(301);
+    }
+    public function restoreByRoleId(){
+        $response = $this->call('put','api/v1_0/permissions/restore/1');
+        $response->assertStatus(200);
+    }
 }
