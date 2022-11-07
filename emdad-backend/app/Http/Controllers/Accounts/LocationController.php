89fb012704dd31c6d6e9a217c9634f7a7861c317
omer.hussien <omer.hussien@nctr.sd>
2022-11-07 12:42:35 +0200

    role and warehouse swagger

--- emdad-backend/app/Http/Controllers/Accounts/LocationController.php
+++ emdad-backend/app/Http/Controllers/Accounts/LocationController.php
@@ -24,47 +24,302 @@ class LocationController extends Controller
     {
         $this->locationService = $locationService;
     }
-
+/**
+        * @OA\Post(
+        * path="/api/v1_0/warehouses/create",
+        * operationId="createWarehouse",
+        * tags={"Account"},
+        * summary="create warehouse",
+        * description="create warehouse Here",
+        *     @OA\RequestBody(
+        *         @OA\JsonContent(),
+        *         @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object",
+        *               required={"warehouseName","warehouseType","location","gateType","receiverName","receiverPhone"},
+        *               @OA\Property(property="warehouseName", type="string"),
+        *               @OA\Property(property="warehouseType", type="string"),
+        *               @OA\Property(property="location", type="integer"),
+        *               @OA\Property(property="gateType", type="string"),
+        *               @OA\Property(property="receiverName", type="string"),
+        *               @OA\Property(property="receiverPhone", type="string")
+        *            ),
+        *        ),
+        *    ),
+        *      @OA\Response(
+        *        response=200,
+        *          description="created Successfully",
+        *          @OA\JsonContent(),
+        *          @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object",
+        *               @OA\Property(property="message", type="string")
+        *            ),
+        *        ),
+        *
+        *       ),
+        *      @OA\Response(response=500, description="system error"),
+        *      @OA\Response(response=422, description="Validate error"),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
     public function saveLocation(CreateLocationRequest $request)
     {
         return $this->locationService->save($request);
     }
-
+/**
+        * @OA\put(
+        * path="/api/v1_0/warehouses/update",
+        * operationId="updateWarehouse",
+        * tags={"Account"},
+        * summary="update warehouse",
+        * description="update warehouse Here",
+        *     @OA\RequestBody(
+        *         @OA\JsonContent(),
+        *         @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object",
+        *               required={"id"},
+        *               @OA\Property(property="id", type="integer"),
+        *               @OA\Property(property="warehouseName", type="string"),
+        *               @OA\Property(property="warehouseType", type="string"),
+        *               @OA\Property(property="location", type="integer"),
+        *               @OA\Property(property="gateType", type="string"),
+        *               @OA\Property(property="receiverName", type="string"),
+        *               @OA\Property(property="receiverPhone", type="string")
+        *            ),
+        *        ),
+        *    ),
+        *      @OA\Response(
+        *        response=200,
+        *          description="updated Successfully",
+        *          @OA\JsonContent(),
+        *          @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object",
+        *               @OA\Property(property="message", type="string")
+        *            ),
+        *        ),
+        *
+        *       ),
+        *      @OA\Response(response=500, description="system error"),
+        *      @OA\Response(response=422, description="Validate error"),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
     public function updateLocation(UpdateLocationRequest $request)
     {
         return $this->locationService->update($request);
     }
-
+/**
+        * @OA\get(
+        * path="/api/v1_0/warehouses/getAll",
+        * operationId="getAllWarehouses",
+        * tags={"Account"},
+        * summary="get all warehouse",
+        * description="get all warehouses Here",
+        *      @OA\Response(
+        *        response=200,
+        *          description="get all warehouses",
+        *          @OA\JsonContent(),
+        *          @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="array",@OA\Items(type = "object")
+        *            ),
+        *        ),
+        *
+        *       ),
+        *      @OA\Response(response=500, description="system error"),
+        *      @OA\Response(response=422, description="Validate error"),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
     public function getAllLocations()
     {
         return $this->locationService->getAll();
     }
-
+/**
+        * @OA\get(
+        * path="/api/v1_0/warehouses/getByCompanyId/{companyId}",
+        * operationId="getByCompanyIdWarehouse",
+        * tags={"Account"},
+        * summary="get warehouse by company id",
+        * description="get warehouse by company id Here",
+        *      @OA\Response(
+        *        response=200,
+        *          description="get warehouse by company id",
+        *          @OA\JsonContent(),
+        *          @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object"
+        *            ),
+        *        ),
+        *
+        *       ),
+        *      @OA\Response(response=500, description="system error"),
+        *      @OA\Response(response=422, description="Validate error"),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
     public function getByLocationByCompanyId(GetByLocationIdRequest $request,$id)
     {
         return $this->locationService->showByCompanyId($id);
     }
-
+/**
+        * @OA\get(
+        * path="/api/v1_0/warehouses/getByUserId/{userId}'",
+        * operationId="getWarehouseUserById",
+        * tags={"Account"},
+        * summary="get warehouse By UserId",
+        * description="get warehouse By UserId Here",
+        *      @OA\Response(
+        *        response=200,
+        *          description="get warehouse By UserId",
+        *          @OA\JsonContent(),
+        *          @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object"
+        *            ),
+        *        ),
+        *
+        *       ),
+        *      @OA\Response(response=500, description="system error"),
+        *      @OA\Response(response=422, description="Validate error"),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
     public function getByLocationByUserId(GetByLocationIdRequest $request,$id)
     {
         return $this->locationService->showByUserId($id);
     }
-
+/**
+        * @OA\get(
+        * path="/api/v1_0/warehouses/getById/{id}'",
+        * operationId="getWarehouseById",
+        * tags={"Account"},
+        * summary="get warehouse By Id",
+        * description="get warehouse By Id Here",
+        *      @OA\Response(
+        *        response=200,
+        *          description="get warehouse By Id",
+        *          @OA\JsonContent(),
+        *          @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object"
+        *            ),
+        *        ),
+        *
+        *       ),
+        *      @OA\Response(response=500, description="system error"),
+        *      @OA\Response(response=422, description="Validate error"),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
     public function getByLocationById(GetByLocationIdRequest $request,$id)
     {
         return $this->locationService->showById($id);
     }
-
+/**
+        * @OA\delete(
+        * path="/api/v1_0/warehouses/delete/{id}'",
+        * operationId="deleteWarehouse",
+        * tags={"Account"},
+        * summary="delete warehouse",
+        * description="delete warehouse Here",
+        *      @OA\Response(
+        *        response=200,
+        *        description="delete warehouse",
+        *          @OA\JsonContent(),
+        *          @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object",
+        *               @OA\Property(property="message", type="string")
+        *            ),
+        *         ),
+        *       ),
+        *      @OA\Response(response=500, description="system error"),
+        *      @OA\Response(response=422, description="Validate error"),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
     public function deleteLocation(GetByLocationIdRequest $request,$id)
     {
         return $this->locationService->delete($id);
     }
-
+/**
+        * @OA\put(
+        * path="/api/v1_0/warehouses/restore/{id}'",
+        * operationId="restoreWarehouseById",
+        * tags={"Account"},
+        * summary="restore warehouse By Id",
+        * description="restore warehouse By Id Here",
+        *      @OA\Response(
+        *        response=200,
+        *          description="restore warehouse By Id",
+        *          @OA\JsonContent(),
+        *          @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object",
+        *               @OA\Property(property="message", type="string")
+        *            ),
+        *          ),
+        *       ),
+        *      @OA\Response(response=500, description="system error"),
+        *      @OA\Response(response=422, description="Validate error"),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
     public function restoreByLocationId(RestoreLocationRequest $request,$id)
     {
         return $this->locationService->restore($id);
     }
-
+/**
+        * @OA\put(
+        * path="/api/v1_0/warehouses/verfied'",
+        * operationId="verfiedWarehouse",
+        * tags={"Account"},
+        * summary="verfied warehouse",
+        * description="verfied warehouse",
+        *     @OA\RequestBody(
+        *         @OA\JsonContent(),
+        *         @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object",
+        *               required={"id","userId","companyId"},
+        *               @OA\Property(property="id", type="integer"),
+        *               @OA\Property(property="userId", type="integer"),
+        *               @OA\Property(property="companyId", type="integer")
+        *            ),
+        *        ),
+        *    ),
+        *      @OA\Response(
+        *        response=200,
+        *          description="verfied warehouse",
+        *          @OA\JsonContent(),
+        *          @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object",
+        *               @OA\Property(property="message", type="string")
+        *            ),
+        *         ),
+        *       ),
+        *      @OA\Response(response=500, description="system error"),
+        *      @OA\Response(response=422, description="Validate error"),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+        * )
+        */
     public function verfiedLocation(VerfiedLocationRequest $request)
     {
         return $this->locationService->verfied($request);
