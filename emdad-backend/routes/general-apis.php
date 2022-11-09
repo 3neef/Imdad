cb78e3389652f5cece500427e1ef29c6133ee4aa
mohamed osman <mohamed.osman@nctr.sd>
2022-11-09 15:35:51 +0200

    add create subscription package with docs

--- emdad-backend/routes/general-apis.php
+++ emdad-backend/routes/general-apis.php
@@ -11,6 +11,7 @@ Route::get('sendSms',[SmsController::class,'sendSms']);
 Route::group(["prefix"=>"subscriptions"], function ( )
 {
     Route::put('update',[SubscriptionController::class,'updateSubscription']);
+    Route::post('create',[SubscriptionController::class,'createPackage']);
     Route::get('get-supplier-packs',[SubscriptionController::class,'getSupplierPackages']);
     Route::get('get-buyer-packs',[SubscriptionController::class,'getBuyerPackages']);
     
