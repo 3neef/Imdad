9cae8c08b8fdaedfd8ca26e2a9e7fea224d6c705
mohamed osman <mohamed.osman@nctr.sd>
2022-11-15 17:39:40 +0200

    remover un necessary columns from company info

--- emdad-backend/database/migrations/2022_11_15_111354_add_first_last_name_to_company_info.php
+++ emdad-backend/database/migrations/2022_11_15_111354_add_first_last_name_to_company_info.php
@@ -15,13 +15,9 @@ return new class extends Migration
     {
         Schema::table('company_info', function (Blueprint $table) {
             //
-            $table->string("first_name",50);
-            $table->string("last_name",50);
-            $table->string("person_id",11);
-            $table->string("id_type",10);
+
             $table->string('company_name')->nullable();
 
-            $table->foreignId('roles_id')->nullable()->references("id")->on("roles")->restrictOnDelete();
 
 
         });
