2d95479dcb4b3c233e1fdc3f1cdfc6afb3d9258d
mohamed osman <mohamed.osman@nctr.sd>
2022-11-15 17:16:53 +0200

    new request to add more company to logged in user

---
+++ emdad-backend/app/Http/Requests/AccountRequests/Account/AddMoreCompanyRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\AccountRequests\Account;
+
+use Dotenv\Validator;
+use Illuminate\Contracts\Validation\Validator as ValidationValidator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class AddMoreCompanyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return auth()->user()!=null;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'crNo' => ['required','exists:related_companies,cr_number'],
+            'companyType' => ['required','integer','between:0,2'],
+           
+        ];
+    }
+
+    protected function failedValidation(ValidationValidator $validator): void
+    {
+        throw new HttpResponseException( response()->json(["errors"=>$validator->errors()],422));
+    }
+}
