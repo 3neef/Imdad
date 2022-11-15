4bcf25837eda83c410811b168099d497882c9248
mohamed osman <mohamed.osman@nctr.sd>
2022-11-15 12:07:26 +0200

    rename column is working please make the following
    composer install
    migrate refresh
    seed

--- emdad-backend/database/migrations/2022_09_26_114553_create_company_infos_table.php
+++ emdad-backend/database/migrations/2022_09_26_114553_create_company_infos_table.php
@@ -16,15 +16,15 @@ return new class extends Migration
         Schema::create('company_info', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string("name",100)->unique();
-            $table->string("logo_path",50)->unique()->nullable(true);
-            $table->string("cr_path",50)->unique()->nullable(true);
-            $table->string("vat_path",50)->unique()->nullable(true);
-            $table->string("company_id",25)->unique();
+            $table->string("name",100)->nullable(true);
+            $table->string("logo_path",90)->unique()->nullable(true);
+            $table->string("cr_path",90)->unique()->nullable(true);
+            $table->string("vat_path",90)->unique()->nullable(true);
+            $table->string("company_id",25)->nullable(true);
             $table->tinyInteger("company_type")->default(0)->comment("0=emdad,1=buyer,2=supplier");
-            $table->string("company_vat_id",25)->unique();
+            $table->string("company_vat_id",25)->nullable(true);
             $table->string("company_cr_id",25)->unique()->nullable(true);
-            $table->string("contact_name",100)->nullable(false);
+            $table->string("contact_name",100)->nullable(true);
             $table->string("contact_phone",15)->nullable(false);
             $table->boolean("is_validated")->default(false);
             $table->string("contact_email",100)->nullable(false);
---
+++ emdad-backend/database/migrations/2022_11_15_111354_add_first_lastname_to_company_info.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('company_info', function (Blueprint $table) {
+            //
+            $table->string("first_name",50);
+            $table->string("last_name",50);
+            $table->string("person_id",11);
+            $table->string("id_type",10);
+            $table->renameColumn('name', 'company_name')->default(null);
+
+            $table->foreignId('roles_id')->nullable(true)->references("id")->on("roles")->restrictOnDelete();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('company_info', function (Blueprint $table) {
+            //
+        });
+    }
+};
