852e7ac9cb08e0568191f50a19b15c2f560ebeb1
=walieldin.hassan <=walieldin.hassan@nctr.sd>
2022-11-17 12:28:57 +0200

    remove redundant code

--- emdad-backend/routes/um-apis.php
+++ emdad-backend/routes/um-apis.php
@@ -62,45 +62,6 @@ Route::middleware(['app_auth', 'auth:sanctum'])->prefix('department')->group(fun
     Route::post('create', [DepartmentController::class, 'create']);
 });
 
-Route::middleware(['app_auth', 'auth:sanctum'])->prefix('users')->group(function () {
-
-        Route::post('createUser', [AuthController::class, 'createUserToCompany']);
-        Route::post('activate', [AuthController::class, 'activateUser']);
-        Route::post('resend-otp', [AuthController::class, 'resendOTP']);
-        Route::post('logout', [AuthController::class, 'logoutUser']);
-        Route::put("update", [AuthController::class, 'updateUser']);
-        Route::delete("delete/{id}", [AuthController::class, 'deleteUser']);
-        Route::put("restore/{id}", [AuthController::class, 'restoreUser']);
-        Route::put("forgot-password", [AuthController::class, 'forgotPassword']);
-        Route::put("reset-password", [AuthController::class, 'resetPassword']);
-        Route::post("assginRole", [AuthController::class, 'assignRole']);
-        Route::post("unAssginRole", [AuthController::class, 'unAssignRole']);
-        Route::post("oldRole", [AuthController::class, 'restoreOldRole']);
-        Route::put("setDefaultCompany", [AuthController::class, 'setDefaultCompany']);
-    });
-
-
-        Route::middleware(['app_auth', 'auth:sanctum'])->prefix('permissions')->group(function () {
-
-        Route::post('save', [PermissionsController::class, 'savePermission']);
-        Route::get('getAll', [PermissionsController::class, 'getAllPermissions']);
-        Route::get('getById/{id}', [PermissionsController::class, 'getPermissionByRoleId']);
-        Route::put('update', [PermissionsController::class, 'updatePermission']);
-        Route::delete('delete/{id}', [PermissionsController::class, 'deletePermission']);
-        Route::put('restore/{id}', [PermissionsController::class, 'restoreById']);
-    });
-
-        Route::middleware(['app_auth', 'auth:sanctum'])->prefix('roles')->group(function () {
-
-        Route::post('save', [RoleController::class, 'saveRole']);
-        Route::get('getAll', [RoleController::class, 'getAllRoles']);
-        Route::get('getByRoleId/{id}', [RoleController::class, 'getByRoleId']);
-        Route::get('getByType/{type}', [RoleController::class, 'getByType']);
-        Route::put('update', [RoleController::class, 'updateRole']);
-        Route::delete('delete/{id}', [RoleController::class, 'deleteRole']);
-        Route::put('restore/{id}', [RoleController::class, 'restoreByRoleId']);
-        Route::get('roles-for-reg', [RoleController::class, 'getRolesForReg']);
-    });
 
 
 
