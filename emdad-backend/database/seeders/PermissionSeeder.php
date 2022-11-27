3a0fff0aa06cb2635812ec66e1d8b8829fd115c7
wali <=walieldin.hassan@nctr.sd>
2022-11-27 08:51:25 +0200

    added permission seeder

--- emdad-backend/database/seeders/PermissionSeeder.php
+++ emdad-backend/database/seeders/PermissionSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\UM\Permission;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PermissionSeeder extends Seeder
 {
@@ -33,30 +34,18 @@ class PermissionSeeder extends Seeder
             ["name"=>"buyer_delivery_view","label"=>"BD2","description"=>""],
             ["name"=>"supplier_rfq_view_accept_reject","label"=>"SR1","description"=>""],
             ["name"=>"supplier_rfq_view_accept","label"=>"SR2","description"=>""],
-            ["name"=>"supplier_rfq_view","label"=>"SR3","description"=>""],
-            ["name"=>"","label"=>"SQ1","description"=>""],
-            ["name"=>"","label"=>"SQ2","description"=>""],
-            ["name"=>"","label"=>"SQ3","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
-            ["name"=>"","label"=>"","description"=>""],
+
         ];
 
         foreach ($permissions as $permission) {
             # code...
-           \App\Models\UM\Permission::factory()->create([
-            "name"=>$permission->name,
-            "label"=>$permission->name,
-            "description"=>$permission->name,
-           ]);
+       
+               DB::table('permissions')->insert([
+                "name" => $permission['name'],
+                "label" => $permission['label'],
+                "description" => $permission['description'],
+                "created_at" => now()
+            ]);
         }
     }
 }
