1993a91447c3b8c5172bfacc0358029e55e5cafa
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2022-11-28 12:47:03 +0200

    added supplier seeder

--- emdad-backend/database/seeders/PermissionSeeder.php
+++ emdad-backend/database/seeders/PermissionSeeder.php
@@ -32,9 +32,35 @@ class PermissionSeeder extends Seeder
             ["name"=>"buyer_invoice_view","label"=>"BI2","description"=>""],
             ["name"=>"buyer_delivery_view_recieve_return","label"=>"BD1","description"=>""],
             ["name"=>"buyer_delivery_view","label"=>"BD2","description"=>""],
-            ["name"=>"supplier_rfq_view_accept_reject","label"=>"SR1","description"=>""],
-            ["name"=>"supplier_rfq_view_accept","label"=>"SR2","description"=>""],
+            ["name"=>"supplier_rfq_reject","label"=>"SRFQ3","description"=>""],
+            ["name"=>"supplier_rfq_accept","label"=>"SRFQ2","description"=>""],
+            ["name"=>"supplier_rfq_view","label"=>"SRFQ1","description"=>""],
             ["name"=>"supplier_quotes_send_cancel_renew_reject","label"=>"SQ3","description"=>""],
+            ["name"=>"supplier_quotes_create_edit","label"=>"SQ2","description"=>""],
+            ["name"=>"supplier_quotes_send_cancel_renew_reject","label"=>"SQ1","description"=>""],
+            ["name"=>"supplier_orders_cancel","label"=>"SPO2","description"=>""],
+            ["name"=>"supplier_orders_view","label"=>"SPO1","description"=>""],
+            ["name"=>"supplier_tax_invoices_view","label"=>"SCL1","description"=>""],
+            ["name"=>"supplier_credit_Agreements_edit","label"=>"SCR3","description"=>""],
+            ["name"=>"supplier_credit_Agreements_invite_accept_reject","label"=>"SCR2","description"=>""],
+            ["name"=>"supplier_credit_Agreements_view","label"=>"SCR1","description"=>""],
+            ["name"=>"supplier_pending_deliveries_return_accept","label"=>"SDD4","description"=>""],
+            ["name"=>"supplier_pending_deliveries_assign_driver_truck","label"=>"SDD3","description"=>""],
+            ["name"=>"supplier_pending_deliveries_requst_edit_accept","label"=>"SDD2","description"=>""],
+            ["name"=>"supplier_pending_deliveries_view","label"=>"SDD1","description"=>""],
+            ["name"=>"supplier_tracking_accept_edit_delivery","label"=>"SAP3","description"=>""],
+            ["name"=>"supplier_tracking_reject_return","label"=>"SAP2","description"=>""],
+            ["name"=>"supplier_tracking_proceding_full_chain_delivery","label"=>"SAP1","description"=>""],
+
+
+
+
+
+
+
+
+
+
 
         ];
 
