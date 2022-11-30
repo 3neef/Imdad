f57765efe9de6c4adce323bf955f07547703a3b8
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2022-11-30 02:16:30 -0800

    doc form create Translation

--- emdad-backend/app/Http/Controllers/Translatoin/TranslationController.php
+++ emdad-backend/app/Http/Controllers/Translatoin/TranslationController.php
@@ -18,7 +18,34 @@ class TranslationController extends Controller
         $this->TranslationService = $TranslationService;
     }
 
-
+/**
+        * @OA\Post(
+        * path="/api/v1_0/translation/create",
+        * operationId="translation create",
+        * tags={"Translation"},
+        * summary="translation create",
+        * description="translation create Here",
+        *     @OA\RequestBody(
+        *         @OA\JsonContent(),
+        *         @OA\MediaType(
+        *            mediaType="multipart/form-data",
+        *            @OA\Schema(
+        *               type="object",
+        *               required={"key","en_value","ar_value"},
+        *               @OA\Property(property="key", type="string"),
+        *               @OA\Property(property="en_value", type="string"),
+        *               @OA\Property(property="ar_value", type="string")
+        *            ),
+        *        ),
+        *    ),
+        *      @OA\Response(
+        *        response=200,
+        *          description="created Successfully"
+        *       ),
+        *      @OA\Response(response=404, description="Resource Not Found"),
+         *      @OA\Response(response=500, description="system error")
+        * )
+        */
     public function create( TranslateRequest $request )
     {
            return $this->TranslationService->create( $request );
