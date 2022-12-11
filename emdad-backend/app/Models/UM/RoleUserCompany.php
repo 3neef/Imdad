8bd5f6dc9328a5ecb865c0dc5cccbb714481bd41
walieldin.hassan <walieldin.hassan@nctr.sd>
2022-12-11 13:52:50 +0200

    added permissons to userRoleCompany

--- emdad-backend/app/Models/UM/RoleUserCompany.php
+++ emdad-backend/app/Models/UM/RoleUserCompany.php
@@ -10,16 +10,21 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class RoleUserCompany extends Pivot
 {
     use SoftDeletes;
-    protected $table ='roles_users_company_info';
-    public function users(){
-        return $this->belongsToMany(User::class,'roles_users_company_info','users_id')->withTimestamps()->withPivot("status");
+    protected $fillable = ['permissions'];
+
+    protected $table = 'roles_users_company_info';
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'roles_users_company_info', 'users_id')->withTimestamps()->withPivot("status");
     }
-    
-    public function roles(){
-        return $this->belongsToMany(Role::class,'roles_users_company_info','roles_id')->withTimestamps()->withPivot("status");
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'roles_users_company_info', 'roles_id')->withTimestamps()->withPivot("status");
     }
-    
-    public function companies(){
-        return $this->belongsToMany(CompanyInfo::class,'roles_users_company_info','company_info_id')->withTimestamps()->withPivot("status");
+
+    public function companies()
+    {
+        return $this->belongsToMany(CompanyInfo::class, 'roles_users_company_info', 'company_info_id')->withTimestamps()->withPivot("status");
     }
 }
--- emdad-backend/database/migrations/2022_12_11_122133_add_permissions_to_roles_users_company_info_table.php
+++ emdad-backend/database/migrations/2022_12_11_122133_add_permissions_to_roles_users_company_info_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->json('permissions')->default("");
+        Schema::table('roles_users_company_info', function (Blueprint $table) {
+            $table->json('permissions')->nullable();
         });
     }
 
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('roles_users_company_info', function (Blueprint $table) {
             //
         });
     }
