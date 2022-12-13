51c089c537a035d7579f831d737bbbc212d5f4ed
walieldin.hassan <walieldin.hassan@nctr.sd>
2022-12-13 10:12:33 +0200

    added active to Profile Ressones

--- emdad-backend/app/Models/User.php
+++ emdad-backend/app/Models/User.php
@@ -66,7 +66,7 @@ class User extends Authenticatable implements MustVerifyEmail
 
     public function currentProfile()
     {
-        return Profile::where("id", $this->profile_id)->select(["name_ar", 'active', 'subscription_details', 'cr_expire_data', 'type', 'profile_id'])->first();
+        return Profile::where("id", $this->profile_id)->select(["name_ar", 'active', 'subscription_details', 'cr_expire_data', 'type', 'id'])->first();
     }
 
     public function assignRole(Role $role)
