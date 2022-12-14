af7aa8bb9288fcd8ef90d4539afd748461edbac2
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2022-12-14 11:27:50 +0200

    generate swagger

--- emdad-backend/app/Http/Controllers/Auth/AuthController.php
+++ emdad-backend/app/Http/Controllers/Auth/AuthController.php
@@ -115,7 +115,7 @@ class AuthController extends Controller
      *               required={"password"},
      *               @OA\Property(
      *                   oneOf={ @OA\Property(property="email",type="email"),
-     *                           @OA\Property(property="mobile",type="string") }),
+     *                          @OA\Property(property="mobile",type="string") }),
      *               @OA\Property(property="password", type="password")
      *            ),
      *        ),
