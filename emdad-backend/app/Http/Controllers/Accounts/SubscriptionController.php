94da7f5abdde6a5f6b30daa2bdc5c968d653779d
sidahmed.faisal <sidahmed.faisal@nctr.sd>
2022-12-19 15:32:35 +0200

    swagger

--- emdad-backend/app/Http/Controllers/Accounts/SubscriptionController.php
+++ emdad-backend/app/Http/Controllers/Accounts/SubscriptionController.php
@@ -231,7 +231,7 @@ class SubscriptionController extends Controller
         * path="/api/v1_0/packages/{id}'",
         * operationId="getpackageById",
         * tags={"Platform Settings"},
-        * summary="get package By Id",
+        * summary="get package By id",
         * description="get package By Id Here",
      *     @OA\Parameter(
      *         name="id",
