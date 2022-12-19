9c3054ef230750d25c6b0dc577505316bc96a760
mohamed osman <mohamed.osman@nctr.sd>
2022-12-19 13:41:08 +0200

    subscriptionId camelCase

--- emdad-backend/app/Http/Controllers/SubscriptionPaymentController.php
+++ emdad-backend/app/Http/Controllers/SubscriptionPaymentController.php
@@ -53,7 +53,7 @@ class SubscriptionPaymentController extends Controller
      *         response=200,
      *         description="",
      *         @OA\JsonContent(
-     *         @OA\Property(property="subscription_id", type="integer", example="{' 'id': 1, 'compnay_id': '1','subscription_id': 1, 'user_id': 1, 'sub_total': 13.0, 'days': 30,'tax_amount':15,'total':28.0}")
+     *         @OA\Property(property="subscriptionId", type="integer", example="{' 'id': 1, 'compnay_id': '1','subscription_id': 1, 'user_id': 1, 'sub_total': 13.0, 'days': 30,'tax_amount':15,'total':28.0}")
      *          ),
      *       )
      *      )
