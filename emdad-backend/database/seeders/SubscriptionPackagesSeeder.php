2c4e278e88763cb625bfc20c77bacf4ba9b24576
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2022-12-21 13:30:14 +0200

    fix seeder package_subscription

--- emdad-backend/database/seeders/SubscriptionPackagesSeeder.php
+++ emdad-backend/database/seeders/SubscriptionPackagesSeeder.php
@@ -19,23 +19,26 @@ class SubscriptionPackagesSeeder extends Seeder
     {
 
         $subscriptionsDetails = [
-            ["id" => 1, "subscription_name" => 'basic', "type" => 2, "subscription_details" => ["superAdmin" => 1, "users" => 21, "paymentMethods" => "cash/credit", "delivery" => "one_time_delivery/standing_order", "warehouses" => 6, "addSuppliers" => 16, "item_in_each_requisition" => 11, "live_tracking" => true, "FreePrice" => "15.000", "BasePrice" => "10000"]],
-            ["id" => 2, "subscription_name" => 'sliver', "type" => 1, "subscription_details" =>
-            ["id" => 3, "superAdmin" => 1, "users" => 20, "paymentMethods" => "cash/credit", "delivery" => "one_time_delivery/standing_order", "warehouses" => 5, "addSuppliers" => 15, "item_in_each_requisition" => 10, "live_tracking" => true, "FreePrice" => "5.000", "BasePrice" => "10000"]],
-            ["id" => 4, "subscription_name" => 'Gold', "type" => 2, "subscription_details" =>
-            ["id" => 5, "superAdmin" => 1, "users" => 5, "paymentMethods" => "cash", "delivery" => "one_time_delivery", "warehouses" => 2, "addSuppliers" => 5, "item_in_each_requisition" => 5, "live_tracking" => false, "FreePrice" => "0", "BasePrice" => "10000"]],
-            ["id" => 6, "subscription_name" => 'basic_2', "type" => 2, "subscription_details" =>
-            ["id" => 7, "superAdmin" => 1, "users" => 5, "paymentMethods" => "cash", "delivery" => "one_time_delivery", "warehouses" => 2, "addSuppliers" => 5, "item_in_each_requisition" => 5, "live_tracking" => false, "FreePrice" => "1.000", "BasePrice" => "10000"]],
+            ["id" => 1, "packageNameAr" => 'الأساسية', "packageNameEn"=>'Basic', "type" => 'Buyer',"price1" => "1250", "price2" => "0",
+             "features" =>
+             [  ["key"=>"delivery","titleEn"=>"Delivery Scheduling","titleAr"=>"التوصيل","systemValue"=>1,"text_en"=>"One Time Delivery","text_ar"=>"التوصيل لمرة واحدة","descriptionEn"=>"lorem epsom",
+            "descriptionAr"=>"تفاصيل شرح الميزة"],
+            ["key"=>"delivery","titleEn"=>"Delivery Scheduling","titleAr"=>"التوصيل","systemValue"=>1,"text_en"=>"One Time Delivery","text_ar"=>"التوصيل لمرة واحدة","descriptionEn"=>"lorem epsom",
+            "descriptionAr"=>"تفاصيل شرح الميزة"]]]   
+        
+        
         ];
 
         foreach ($subscriptionsDetails as $subscriptionsDetail) {
             DB::table('subscription_packages')->insert([
                 "id" => $subscriptionsDetail["id"],
-                "subscription_name" => $subscriptionsDetail['subscription_name'],
-                "type" => $subscriptionsDetail['type'],
-                "subscription_details" => json_encode($subscriptionsDetail['subscription_details'], true),
+                "package_name_ar" => $subscriptionsDetail["packageNameAr"],
+                "package_name_en" => $subscriptionsDetail["packageNameEn"],
+                "type" => $subscriptionsDetail["type"],
+                "price_1" => $subscriptionsDetail["price1"],
+                "price_2" => $subscriptionsDetail["price2"],
+                "features" => json_encode($subscriptionsDetail['features'], true),
                 "created_at" => Carbon::now(),
-
             ]);
         }
     }
