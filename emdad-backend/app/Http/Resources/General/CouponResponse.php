2d42376bdb24587a28b2fc2ccbec55f4565762ca
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2022-12-23 21:17:47 +0200

    Coupon, Departmenrt and Permission Responses

---
+++ emdad-backend/app/Http/Resources/General/CouponResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\General;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DepartmentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'allowed'=>$this->allowed,
+            'startDate'=>$this->start_date,
+            'endDate'=>$this->end_date,
+            'value'=>$this->value,
+            'isPercentage'=>$this->is_percentage,
+            'code'=>$this->code
+        ];
+    }
+}
