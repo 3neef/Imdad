9a9f1006adf9ac67d4214cc18f23d7fa92ef39cf
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2022-12-25 20:50:05 +0200

    doc get profile user

--- emdad-backend/app/Http/Controllers/User/UserController.php
+++ emdad-backend/app/Http/Controllers/User/UserController.php
@@ -28,6 +28,60 @@ class UserController extends Controller
     {
         return UserCollection::collection($request);
     }
+/**
+     * @OA\get(
+     * path="/api/v1_0/users/get-users",
+     * operationId="get-users",
+     * tags={"UM & Permissions"},
+     * summary="get-users ",
+     * description="get-users Here",
+     *     @OA\Parameter(
+     *         name="x-authorization",
+     *         in="header",
+     *         description="Set x-authorization",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *         *     @OA\Parameter(
+     *         name="token",
+     *         in="header",
+     *         description="Set user authentication token",
+     *         @OA\Schema(
+     *             type="beraer"
+     *         )
+     *     ),
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(),
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *            ),
+     *        ),
+     *    ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="User created successfully",
+     *          @OA\JsonContent(),
+     *          @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               @OA\Property(property="message", type="string"),
+     *               @OA\Property(property="data", type="object")
+     *            ),
+     *          ),
+     *       ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(response=400, description="Bad request"),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     * )
+     */
 
     public function getProfileUsers(Request $request)
     {
