537ff46f7fcf978e9c869e9bd77e8099aedfba9c
sidahmed.faisal <sidahmed.faisal@nctr.sd>
2022-12-31 15:34:21 +0200

    retryApproval

--- emdad-backend/app/Http/Services/CategoryServices/CategoryService.php
+++ emdad-backend/app/Http/Services/CategoryServices/CategoryService.php
@@ -5,6 +5,7 @@ namespace App\Http\Services\CategoryServices;
 use App\Http\Collections\CategoryCollection;
 use App\Http\Resources\CategoryResourses\category\CategoryResource;
 use App\Models\Emdad\Categories;
+use App\Models\ProfileCategoryPivot;
 use Exception;
 
 class CategoryService
@@ -116,10 +117,13 @@ class CategoryService
 
     }
 
-    public function RetryApproval($id){
-        $category = Categories::where('id', $id)->first();
-        if (auth()->user()->profile_id && $category->status=="rejected") {
-            
+    public function RetryApproval($request){
+        $category = ProfileCategoryPivot::where('category_id', $request->id)->where('Profile_id',auth()->user()->profile_id)->first();
+        if ($category->status=="rejected") {
+            $category->update([
+                "status" => "pending",
+                "reason" => $request->reason ?? $category->reason,
+            ]);
         }
         
     }
