c993d78353c930315b9f1f3dacf08d7ccd79705a
sidahmed.faisal <sidahmed.faisal@nctr.sd>
2022-12-31 16:28:37 +0200

    add endpoints

--- emdad-backend/routes/category-apis.php
+++ emdad-backend/routes/category-apis.php
@@ -18,7 +18,8 @@ Route::middleware(['auth.apikey', 'auth:sanctum'])->group(function () {
 Route::middleware(['auth.apikey', 'auth:sanctum'])->group(function () {
     Route::apiResource('categories', CategoryController::class);
     Route::put('categories/restore/{id}', [CategoryController::class, 'restore']);
-    Route::put('categories/RetryApproval/{id}', [CategoryController::class, 'reset']);
+    Route::put('categories/RetryApproval/{id}', [CategoryController::class, 'RetryRejectedCategories']);
+    Route::put('categories/changeCategoryStatus/{id}', [CategoryController::class, 'changeCategoryStatus']);
     Route::post('categories/company-categories', [CategoryController::class, 'setFavoriteCategories']);
     Route::post('categories/approveCategory', [CategoryController::class, 'approveCategory']);
     Route::post('categories/rejectCategory', [CategoryController::class, 'rejectCategory']);
