4a1be950b6a8b205e7748dc7d226fcde541969c9
mohamed osman <mohamed.osman@nctr.sd>
2023-01-03 11:03:46 +0200

    test dev pipline

--- emdad-backend/database/seeders/DatabaseSeeder.php
+++ emdad-backend/database/seeders/DatabaseSeeder.php
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //small change to test pipline
     }
 }
