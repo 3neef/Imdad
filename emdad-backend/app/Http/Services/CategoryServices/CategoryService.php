12399bde9e2817b1d3ced022e2841a032280d040
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2023-01-07 14:48:12 +0200

    test

--- emdad-backend/app/Http/Services/CategoryServices/CategoryService.php
+++ emdad-backend/app/Http/Services/CategoryServices/CategoryService.php
@@ -115,6 +115,7 @@ class CategoryService
         $category = Categories::first();
         if (isset($request['categoryList'])) {
             foreach ($request['categoryList'] as $category_id) {
+      
                 try {
                     $category->companyCategory()->attach($category->id, ['category_id' => $category_id, 'profile_id' => auth()->user()->profile_id,'created_by'=>auth()->id()]);
                 } catch (Exception $e) {
