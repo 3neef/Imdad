6d99fabb6f475f0165c9675a92b99f92a1a0c3cd
sidahmed.faisal <sidahmed.faisal@nctr.sd>
2023-01-11 17:54:10 +0200

    chage validation status codes

--- emdad-backend/app/Http/Requests/UMRequests/Permission/UpdatePermissionRequest.php
+++ emdad-backend/app/Http/Requests/UMRequests/Permission/UpdatePermissionRequest.php
@@ -40,6 +40,6 @@ class UpdatePermissionRequest extends FormRequest
 
     protected function failedValidation(Validator $validator): void
     {
-        throw new HttpResponseException( response()->json(["success"=>false,"errors"=>$validator->errors()],422));
+        throw new HttpResponseException(  response()->json(["success" => false, "errors" => $validator->errors(),"statusCode"=>"422"], 200));
     }
 }
