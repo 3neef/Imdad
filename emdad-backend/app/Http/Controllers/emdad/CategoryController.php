9bd47765393855f584001d0d340ea8c6a031aa95
sidahmed.faisal <sidahmed.faisal@nctr.sd>
2023-01-11 09:54:39 +0200

    swagger

--- emdad-backend/app/Http/Controllers/emdad/CategoryController.php
+++ emdad-backend/app/Http/Controllers/emdad/CategoryController.php
@@ -79,7 +79,7 @@ class CategoryController extends Controller
 
     /**
      * @OA\get(
-     *    path="/api/v1_0/categories",
+     *    path="/api/v1_0/categories/{id}",
      *    operationId="showallcatogre",
      *    tags={"Catogry"},
      *    summary="show all catogries on the user profile",
@@ -92,7 +92,7 @@ class CategoryController extends Controller
      *             type="string"
      *         )
      *     ),
-     *         *     @OA\Parameter(
+     *     @OA\Parameter(
      *         name="token",
      *         in="header",
      *         description="Set user authentication token",
@@ -304,7 +304,7 @@ class CategoryController extends Controller
     }
     /**
      * @OA\get(
-     * path="/api/v1_0/categories/{id}",
+     * path="/api/v1_0/categories/{Id}",
      * operationId="getBycategoryId",
      * tags={"Catogry"},
      * summary="get By categoryId",
