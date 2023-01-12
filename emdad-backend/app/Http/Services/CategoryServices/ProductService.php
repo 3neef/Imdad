4627b698c644fa8d7df110117d469ba9b1ab9385
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2023-01-12 12:52:32 +0200

    tst server

--- emdad-backend/app/Http/Services/CategoryServices/ProductService.php
+++ emdad-backend/app/Http/Services/CategoryServices/ProductService.php
@@ -63,6 +63,7 @@ class ProductService
 
         // attach the  product to the  meida collection  and update  product 
 
+        
         $product->addMultipleMediaFromRequest(['attachementFile'])
             ->each(function ($fileAdder) {
                 $fileAdder->toMediaCollection('products');
