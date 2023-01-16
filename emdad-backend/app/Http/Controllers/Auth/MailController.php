1942bd73517c4147d610a4d9dbb68af4b77fc8b3
mohamed osman <mohamed.osman@nctr.sd>
2023-01-16 16:16:50 +0200

    mail

--- emdad-backend/app/Http/Controllers/Auth/MailController.php
+++ emdad-backend/app/Http/Controllers/Auth/MailController.php
@@ -31,7 +31,7 @@ class MailController extends Controller
             'name' => $name,
             'email' => $email,
             'lang' => $lang,
-            'link' => "http://172.21.1.116:8080/reset-password?email=" . $email . "&token=" . $token->token
+            'link' => "http://172.21.1.116:9090/reset-password?email=" . $email . "&token=" . $token->token
         ];
         // dd($data);
 
