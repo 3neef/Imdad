c331278e6dad2eac4d75806bbd4f703fa748ed76
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2023-01-24 13:05:29 +0200

    user create by admin email template  EN

--- emdad-backend/resources/views/mail/user-created/en.blade.php
+++ emdad-backend/resources/views/mail/user-created/en.blade.php
@@ -1,1 +1,3 @@
-Your One-time passcode (OTP) is: <span style="color:blue;">{{ $viewData['otp'] }}</span>
+<div dir="ltr">
+    An account has been created for you by {{$viewData['var']['admin']}}  on Emdad platform  please use the following temperory password  <span style="color:blue;">{{ $viewData['var']['password'] }}</span>
+</div>
