0c38bd63863b807dac9a11493e63d7c6c774df9b
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2023-01-24 14:37:15 +0200

    controller and service , Request for Model model

--- emdad-backend/routes/emdadSetting-apis.php
+++ emdad-backend/routes/emdadSetting-apis.php
@@ -2,15 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Settings\BrandController;
+use App\Http\Controllers\Settings\ColorController;
+use App\Http\Controllers\Settings\ModelController;
 
 Route::middleware(['auth.apikey', 'auth:sanctum'])->group(function () {
     Route::apiResource('brands', BrandController::class);
 });
 
 Route::middleware(['auth.apikey', 'auth:sanctum'])->group(function () {
-    Route::apiResource('colors', BrandController::class);
+    Route::apiResource('colors', ColorController::class);
 });
 
 Route::middleware(['auth.apikey', 'auth:sanctum'])->group(function () {
-    Route::apiResource('models', BrandController::class);
+    Route::apiResource('models', ModelController::class);
 });
