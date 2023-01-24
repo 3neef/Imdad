8a7c4896d543a1838f3f39f4ae73e8a278b06b9e
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2023-01-24 14:46:29 +0200

    controller and service , Request for Size model

--- emdad-backend/app/Http/Controllers/Settings/SizeController.php
+++ emdad-backend/app/Http/Controllers/Settings/SizeController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers\Settings;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Settings\AddSizeRequest;
+use App\Http\Services\Settings\SizeService;
 use Illuminate\Http\Request;
 
 class SizeController extends Controller
 {
-    //
+    public SizeService $sizeServices;
+
+
+    public function __construct(SizeService $sizeServices) {
+        $this->sizeServices = $sizeServices;
+    }
+
+
+    public function store(AddSizeRequest $request)
+    {
+        $size = $this->sizeServices->store($request);
+        if($size){
+            return response()->json(["statusCode" => "000",'message' => 'created successfully', ], 200);
+        }
+        return response()->json([ "statusCode" => "264",'success' => false, 'message' => "User Dosn't belong to any profile "], 200);
+    }
+    
+
+    public function index(Request $request)
+    {
+        $size = $this->sizeServices->show($request);
+        if($size){
+            return response()->json(["statusCode" => "000",'data' => $size,
+            ], 200);
+        }
+        return response()->json([
+            "statusCode" => "264",'success' => false, 'message' => "User Dosn't belong to any profile "], 200);
+    }
+
 }
--- emdad-backend/routes/emdadSetting-apis.php
+++ emdad-backend/routes/emdadSetting-apis.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Settings\BrandController;
 use App\Http\Controllers\Settings\ColorController;
 use App\Http\Controllers\Settings\ModelController;
+use App\Http\Controllers\Settings\SizeController;
 
 Route::middleware(['auth.apikey', 'auth:sanctum'])->group(function () {
     Route::apiResource('brands', BrandController::class);
@@ -15,4 +16,10 @@ Route::middleware(['auth.apikey', 'auth:sanctum'])->group(function () {
 
 Route::middleware(['auth.apikey', 'auth:sanctum'])->group(function () {
     Route::apiResource('models', ModelController::class);
+});
+
+
+
+Route::middleware(['auth.apikey', 'auth:sanctum'])->group(function () {
+    Route::apiResource('sizes', SizeController::class);
 });
