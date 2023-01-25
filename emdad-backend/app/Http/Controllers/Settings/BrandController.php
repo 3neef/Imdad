706c7a895fc5467ac141446de5f9ea70c4d26bee
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2023-01-25 09:56:58 +0200

    brands  swagger

--- emdad-backend/app/Http/Controllers/Settings/BrandController.php
+++ emdad-backend/app/Http/Controllers/Settings/BrandController.php
@@ -16,7 +16,39 @@ class BrandController extends Controller
         $this->prandServices = $prandServices;
     }
 
-
+ /**
+     * @OA\post(
+     *    path="/api/v1_0/brands",
+     *    operationId="storebrands",
+     *    tags={"EmdadSettings"},
+     *    summary="store brands",
+     *    description="store brands Here",
+     *     @OA\Parameter(
+     *         name="x-authorization",
+     *         in="header",
+     *         description="Set x-authorization",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *        @OA\Parameter(
+     *         name="token",
+     *         in="header",
+     *         description="Set user authentication token",
+     *         @OA\Schema(
+     *             type="beraer"
+     *         )
+     *     ),
+     *    @OA\Response(
+     *         response=200,
+     *         description="",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="message",  example="{statusCode => 000 ,message => create successfully }200")
+     *          ),
+     *       )
+     *      )
+     *  )
+     */
     public function store(AddBrandRequest $request)
     {
         $brand = $this->prandServices->store($request);
@@ -26,6 +58,39 @@ class BrandController extends Controller
         return response()->json([ "statusCode" => "264",'success' => false, 'message' => "Brand Canot create "], 200);
     }
     
+ /**
+     * @OA\get(
+     *    path="/api/v1_0/brands",
+     *    operationId="showallbrands",
+     *    tags={"EmdadSettings"},
+     *    summary="show all brands",
+     *    description="show all brands Here",
+     *     @OA\Parameter(
+     *         name="x-authorization",
+     *         in="header",
+     *         description="Set x-authorization",
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *        @OA\Parameter(
+     *         name="token",
+     *         in="header",
+     *         description="Set user authentication token",
+     *         @OA\Schema(
+     *             type="beraer"
+     *         )
+     *     ),
+     *    @OA\Response(
+     *         response=200,
+     *         description="",
+     *         @OA\JsonContent(
+     *         @OA\Property(property="message",  example="{statusCode => 000,data => $brand}200")
+     *          ),
+     *       )
+     *      )
+     *  )
+     */
 
     public function index(Request $request)
     {
