dc17999421eaf33064dc14299160901af17916d3
sidahmed.faisal <sidahmed.faisal@nctr.sd>
2023-01-25 10:05:10 +0200

    nationality model and migration

--- emdad-backend/database/migrations/2023_01_24_083910_create_drivernationalities_table.php
+++ emdad-backend/database/migrations/2023_01_24_083910_create_drivernationalities_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('drivernationalities', function (Blueprint $table) {
             $table->id();
+            $table->string('name_en');
+            $table->string('name_ar');
             $table->timestamps();
         });
     }
