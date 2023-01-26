6e43db388b76b47838ab8988a7873137010e3812
walieldin.hassan <walieldin.hassan@nctr.sd>
2023-01-26 17:28:58 +0200

    products

--- emdad-backend/app/Http/Resources/CategoryResourses/Product/ProductResponse.php
+++ emdad-backend/app/Http/Resources/CategoryResourses/Product/ProductResponse.php
@@ -25,7 +25,7 @@ class ProductResponse extends JsonResource
             'measruingUnit' => $this->measruing_unit,
             'profileId' => $this->profile_id,
             'categoryId' => $this->category_id,
-            "categoryType" => $this->category->type,
+            "categoryType" => $this->category->type??'',
             'creatorName' => $this->creatorName()->full_name,
             "ProductsImages" => $this->getMedia('products'),
             "CategorySequence" => $this->category->sequence(),
