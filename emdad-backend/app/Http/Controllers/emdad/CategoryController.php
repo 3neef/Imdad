f9e1e66b21fdf94b473ede2079e065cbad93ad16
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2023-01-31 17:23:41 +0200

    update statusCode

--- emdad-backend/app/Http/Controllers/emdad/CategoryController.php
+++ emdad-backend/app/Http/Controllers/emdad/CategoryController.php
@@ -214,7 +214,7 @@ class CategoryController extends Controller
         $category =  $this->categoryService->destroy($id);
 
         if ($category) {
-            return response()->json(['message' => 'deleted successfully', 'statusCode'=>112], 301);
+            return response()->json(['message' => 'Deleted Successfully', 'statusCode'=>112], 301);
         } else {
             return response()->json(['success' => false, 'error' => 'not found', 'statusCode'=>111], 404);
         }
