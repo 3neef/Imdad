ff06d7d48e13bec24575228a16ecb22fdb511e34
murtada.abdalgalil <murtada.abdalgalil@nctr.sd>
2023-02-01 09:34:27 +0200

    UOM seeder

--- emdad-backend/database/seeders/UOMSeeder.php
+++ emdad-backend/database/seeders/UOMSeeder.php
@@ -20,9 +20,22 @@ class UOMSeeder extends Seeder
 
 
         $unit_of_measuerments = [
-            ["id" => 1, "name_ar" => 'كيلو متر', "name_en" => "kilometer", "symbol" => "KM", "relation" => "%", "value" => "1000", "related_unit" => 0],
-            ["id" => 2, "name_ar" => 'متر', "name_en" => "meter", "symbol" => "M", "relation" => "*", "value" => "1000", "related_unit" => 1],
-            ["id" => 3, "name_ar" => 'سنتمتر', "name_en" => "centimeters", "symbol" => "CM", "relation" => "*", "value" => "100", "related_unit" => 2]
+            ["id" => 1, "name_ar" => 'حبة ', "name_en" => "Each", "symbol" => "EA", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 2, "name_ar" => 'عبوة ', "name_en" => "Bottle", "symbol" => "BO", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 3, "name_ar" => 'لتر ', "name_en" => "Litre", "symbol" => "LTR", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 4, "name_ar" => 'صندوق ', "name_en" => "Box", "symbol" => "BX", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 5, "name_ar" => 'كرتون ', "name_en" => "Carton", "symbol" => "CT", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 6, "name_ar" => 'كيس ', "name_en" => "Bag", "symbol" => "BAG", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 7, "name_ar" => 'غرام ', "name_en" => "Gram", "symbol" => "G", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 8, "name_ar" => 'كيلوغرام ', "name_en" => "Kilogram", "symbol" => "KG", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 9, "name_ar" => 'دستة ', "name_en" => "Dozen", "symbol" => "DZ", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 10, "name_ar" => 'طقم ', "name_en" => "Set", "symbol" => "SET", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 11, "name_ar" => 'عدة ', "name_en" => "Kit", "symbol" => "KIT", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 12, "name_ar" => 'حزمة ', "name_en" => "Pack", "symbol" => "PAC", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 13, "name_ar" => 'كيس ', "name_en" => "Sack", "symbol" => "SAK", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 14, "name_ar" => 'لفة ', "name_en" => "Roll", "symbol" => "RO", "relation" => "%", "value" => "1000", "related_unit" => 0],
+            ["id" => 15, "name_ar" => 'زوج ', "name_en" => "Pair", "symbol" => "PR", "relation" => "%", "value" => "1000", "related_unit" => 0],
+
         ];
         foreach ($unit_of_measuerments as $unit_of_measuerment) {
             DB::table('unit_of_measures')->insert([
